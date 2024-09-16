18359eaa4d64098f4d3464dff151562e9d63c463
Ani Laliashvili <alaliashvili@teslaforecast.com>
2024-09-16 15:30:52 -0400

    Added outage class

---
+++ CoreLibrary/CoreLibrary/Outage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLibrary
+{
+    public class Outage
+    {
+        public DateTime StartTime { get; set; } // Outage start time
+        public DateTime EndTime { get; set; } // Outage end time
+        public string Reason { get; set; } = ""; // Optional reason for the outage
+    }
+}
