2f6f9940151d5a32334f7df953a60fb82ddb5694
Ani Laliashvili <alaliashvili@teslaforecast.com>
2024-09-16 13:39:44 -0400

    Added Program.cs

---
+++ CoreLibrary/CoreLibrary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoreLibrary
+{
+    public class Program
+    {
+        // Entry point of the application
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the CoreLibrary application!");
+            // Your code here, or call other classes/methods to start the program.
+        }
+    }
+}
