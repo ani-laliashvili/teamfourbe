00aa1e0b476279b04a2fe012696fc27b38a6b0cc
Asaad Mohammad <amohammad@teslaforecast.com>
2024-09-17 09:41:07 +1200

    price

---
+++ CoreLibrary/CoreLibrary/PriceForecast.cs
@@ -0,0 +1,28 @@
+﻿namespace CoreLibrary
+{
+    public class PriceForecast
+    {
+        public static double[] CreatePriceForecast(int numTimeSlots)
+        {
+            double[] P_price = new double[numTimeSlots];
+
+            for (int h = 0; h < numTimeSlots; h++)
+            {
+                if ((h >= 0 && h <= 6) || (h >= 22 && h <= 23))
+                {
+                    P_price[h] = 0.10; // Low price period
+                }
+                else if (h >= 17 && h <= 20)
+                {
+                    P_price[h] = 0.30; // Peak price period
+                }
+                else
+                {
+                    P_price[h] = 0.20; // Regular price
+                }
+            }
+
+            return P_price;
+        }
+    }
+}
